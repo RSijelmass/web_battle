2f2a0185ac6e1ce5fe362ba4ba752046f8e5fc44
Sam Pritchard <sampritchard@msn.com>
2017-05-26 10:25:46 +0100

    removed player global variables and replaced with game global variables

--- app.rb
+++ app.rb
@@ -11,21 +11,22 @@ class Battle < Sinatra::Base
   end
 
   post '/names' do
-    $player_1 = Player.new(params[:player1])
-    $player_2 = Player.new(params[:player2])
+    @player1 = Player.new(params[:player1])
+    @player2 = Player.new(params[:player2])
+    $game = Game.new(@player1, @player2)
     redirect to '/play'
   end
 
   get '/play' do
-    @player1 = $player_1
-    @player2 = $player_2
+    @player1 = $game.player1
+    @player2 = $game.player2
     erb :play
   end
 
   get '/attack' do
-    @player1 = $player_1
-    @player2 = $player_2
-    Game.new(@player1,@player2).attack(@player2)
+    @player1 = $game.player1
+    @player2 = $game.player2
+    $game.attack(@player2)
     erb :attack
     redirect '/play'
   end
