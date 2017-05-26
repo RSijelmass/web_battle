0b1fe68a4c8ca6105868ce605fc6e40638b93df4
Sam Pritchard <sampritchard@msn.com>
2017-05-26 11:51:19 +0100

    allowed player 2 HP to be reduced when attacked. Tests created and passed

--- spec/features/player2_hit_points_spec.rb
+++ spec/features/player2_hit_points_spec.rb
@@ -5,4 +5,16 @@ feature 'Main' do
     expect(page).to have_content('Sam Hitpoints: 100')
   end
 
+  scenario 'player 1 reduces player 2 hit points' do
+    sign_in_and_play
+    click_button('Attack')
+    expect(page).to have_content('Sam Hitpoints: 90')
+  end
+
+  scenario 'player 2 reduces player 1 hit points' do
+    sign_in_and_play
+    click_button('Switch')
+    click_button('Attack')
+    expect(page).to have_content('Ian Hitpoints: 90')
+  end
 end
