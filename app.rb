7180a3b8478ecc014bd03ba074528b57e49f250f
RSijelmass <R.Sijelmass@gmail.com>
2017-05-26 15:59:00 +0100

    Removed last $game in /names

--- app.rb
+++ app.rb
@@ -13,7 +13,7 @@ class Battle < Sinatra::Base
   post '/names' do
     @player1 = Player.new(params[:player1])
     @player2 = Player.new(params[:player2])
-    $game = Game.new(@player1, @player2)
+    game = Game.new(@player1, @player2)
     redirect to '/play'
   end
 
